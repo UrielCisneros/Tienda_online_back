b259f29100d1646ae90951054fefb9daccecf9bf
UrielCisneros <urielcisne96@gmail.com>
2020-10-06 15:18:07 -0500

    completando envio de correos

--- src/controllers/apartado.controllers.js
+++ src/controllers/apartado.controllers.js
@@ -73,15 +73,6 @@ apartadoCtrl.agregarApartado = async (req, res) => {
 	}
 
 	const contentHTML = `
-	<!DOCTYPE html>
-	<html lang="en">
-		<head>
-			<meta charset="UTF-8">
-			<meta name="viewport" content="width=device-width, initial-scale=1.0">
-			<title>Document</title>
-			<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
-		</head>
-		<body>
 			<h1 style="text-align: center;  font-family: sans-serif;">Parece que tienes una nueva solicitud de apartado</h1>
 	
 			<div class="" style="display: flex;">
@@ -93,9 +84,6 @@ apartadoCtrl.agregarApartado = async (req, res) => {
 				</div>
 				<div class="" style="position: relative;"></div>
 			</div>
-		</body>
-	
-	</html>
 	`;
 
 	const transporter = nodemailer.createTransport({
