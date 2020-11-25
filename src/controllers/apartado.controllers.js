a43dba4c88592e51724a465abaefed86fbe38484
Diego <consultoresartificialbrain@gmail.com>
2020-11-25 15:49:16 -0600

    agregando apartado multiple

--- src/controllers/apartado.controllers.js
+++ src/controllers/apartado.controllers.js
@@ -253,16 +253,18 @@ apartadoCtrl.createApartadoMultiple = async (req,res) => {
 	
 			<h3 style="text-align: center;  font-family: sans-serif; margin: 15px 15px; font-weight: bold;">Detalle del apartado:</h3>
 			<div style="box-shadow: 0 4px 8px 0 rgba(0,0,0,0.5);transition: 0.3s; width: 350px; display:block;">
-				<table style="display:block; margin:auto;">
-					<tr>
-						<td style="  padding: 15px; text-align: left;"><strong>Producto</strong></td>
-						<td style="  padding: 15px; text-align: left;"><strong></strong></td>
-						<td style="  padding: 15px; text-align: left;"><strong>Cantidad</strong></td>
-						<td style="  padding: 15px; text-align: left;"><strong>Medida</strong></td>
-						<td style="  padding: 15px; text-align: left;"><strong>Precio</strong></td>
-					</tr>
-					${pedidos}
-				</table>
+				<div style="margin:auto; max-width: 550px;">
+					<table style="display:block; margin:auto;">
+						<tr>
+							<td style="  padding: 15px; text-align: left;"><strong>Producto</strong></td>
+							<td style="  padding: 15px; text-align: left;"><strong></strong></td>
+							<td style="  padding: 15px; text-align: left;"><strong>Cantidad</strong></td>
+							<td style="  padding: 15px; text-align: left;"><strong>Medida</strong></td>
+							<td style="  padding: 15px; text-align: left;"><strong>Precio</strong></td>
+						</tr>
+						${pedidos}
+					</table>
+				</div>
 			</div>
 		</div>
 		`;
