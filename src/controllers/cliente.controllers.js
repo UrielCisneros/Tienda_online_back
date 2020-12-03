9b423a48952ec1ed86a60c93692e98197cce82a6
Diego <consultoresartificialbrain@gmail.com>
2020-12-03 15:57:16 -0600

    log en auth

--- src/controllers/cliente.controllers.js
+++ src/controllers/cliente.controllers.js
@@ -540,26 +540,31 @@ clienteCtrl.authCliente = async (req, res, next) => {
 			if (!cliente) {
 				res.status(404).json({ message: 'Este usuario no existe' });
 			} else {
-				if (!bcrypt.compareSync(contrasena, cliente.contrasena)) {
-					console.log('entro');
-					res.status(500).json({ message: 'Contraseña incorrecta' });
-					next();
-				} else {
-					const token = jwt.sign(
-						{
-							email: cliente.email,
-							nombre: cliente.nombre,
-							apellido: cliente.apellido,
-							_id: cliente._id,
-							tipoSesion: "APIRestAB",
-							imagen: cliente.imagen,
-							rol: false
-						},
-						process.env.AUTH_KEY
-					);
-					//token
-					res.json({ token });
+				if(cliente.tipoSesion !== "APIRestAB"){
+					if (!bcrypt.compareSync(contrasena, cliente.contrasena)) {
+						console.log('entro');
+						res.status(500).json({ message: 'Contraseña incorrecta' });
+						next();
+					} else {
+						const token = jwt.sign(
+							{
+								email: cliente.email,
+								nombre: cliente.nombre,
+								apellido: cliente.apellido,
+								_id: cliente._id,
+								tipoSesion: "APIRestAB",
+								imagen: cliente.imagen,
+								rol: false
+							},
+							process.env.AUTH_KEY
+						);
+						//token
+						res.json({ token });
+					}
+				}else{
+					res.status(500).json({ message: 'Este usuario esta registrado con firebase.', err });
 				}
+
 			}
 		} catch (err) {
 			console.log(err);
